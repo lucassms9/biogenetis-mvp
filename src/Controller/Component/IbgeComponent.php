ace0cf58e86080b548866464272e7722f2310987
Lucas Santos <lucassms9@hotmail.com>
2021-02-05 18:15:10 -0300

    disabled ibge

--- src/Controller/Component/IbgeComponent.php
+++ src/Controller/Component/IbgeComponent.php
@@ -16,7 +16,7 @@ class IbgeComponent extends Component
 
     public function getStates()
     {
-
+        return [];
         $response = $this->client->get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome', [
             'headers' => ['Content-Type' => 'application/json', ]
         ]);
@@ -26,7 +26,7 @@ class IbgeComponent extends Component
 
     public function getCity($uf)
     {
-
+        return [];
         $response = $this->client->get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/'.$uf.'/municipios?orderBy=nome', [
             'headers' => ['Content-Type' => 'application/json', ]
         ]);
@@ -36,7 +36,7 @@ class IbgeComponent extends Component
 
     public function getStateById($id)
     {
-
+        return [];
         $response = $this->client->get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/'.$id, [
             'headers' => ['Content-Type' => 'application/json', ]
         ]);
@@ -45,7 +45,7 @@ class IbgeComponent extends Component
     }
     public function getCityById($id)
     {
-
+        return [];
         $response = $this->client->get('https://servicodados.ibge.gov.br/api/v1/localidades/municipios/'.$id, [
             'headers' => ['Content-Type' => 'application/json', ]
         ]);
